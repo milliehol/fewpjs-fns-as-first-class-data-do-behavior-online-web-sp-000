1131e8f04fcc660338d88014391b31e3c5e01e31
Carla Holcomb <carla.programmer@gmail.com>
2020-05-29 23:38:17 +0000

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+ 
+function greet(time) {
+  let timeN = parseInt(time, 10)
+  if (timeN <= 12) {
+    return `Good Morning`;
+  }
+  else if (timeN > 17){
+    return 'Good Evening';
+  }
+   else {
+    return 'Good Afternoon';
+  }
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(message) {
+ document.getElementById("greeting").innerText = message;
+}
